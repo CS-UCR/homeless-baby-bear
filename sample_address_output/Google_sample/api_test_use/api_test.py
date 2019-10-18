5b69079f127abea50542077d775db4811a72f335
Jia Xie <jxie031@ucr.edu>
2019-10-18 13:50:18 -0700

    update_more_accuracy

--- sample_address_output/Google_sample/api_test_use/api_test.py
+++ sample_address_output/Google_sample/api_test_use/api_test.py
@@ -1,4 +1,5 @@
 import string
+import re
 def detect_document( i,path):
     """Detects document features in an image."""
     from google.cloud import vision
@@ -16,40 +17,74 @@ def detect_document( i,path):
     for page in response.full_text_annotation.pages:
         for block in page.blocks:
             for paragraph in block.paragraphs:
-                word_arr = []
                 for word in paragraph.words:
                     word_text = ''.join([
                         symbol.text for symbol in word.symbols
                     ])
                     if word_text not in string.punctuation:
-                        word_arr.append(word_text)
+                        text_arr.append(word_text)
                 col_count += 1
-            text_arr += word_arr
+    print(text_arr)
     i = i+1
     return [i,text_arr]
 
 
 def clean_data(arr, text_arr):
     cleaned = []
+    states = []
+    with open("states.txt") as f:
+        for line in f:
+            for word in line:
+                states.append(word)
     file = open("result.txt", "w")
     for doc in text_arr:
-        last_digit = 0
+        digits = [0]
         address = ""
+        if len(doc) < 8:
+            for element in doc:
+                address += element + ' '
+            file.write(address+"\n")
+            continue
+        flag = 0
         for word in doc:
-            if len(word) == 5 and word.isdigit():
-                temp_arr = doc[last_digit:doc.index(word)+1]
+            if word in states and doc[doc.index(word)+1].isdigit() and doc.index(word) > 5:
+                flag = 1
+            if word.isalpha():
+                if (word == "ave" or word == "Ave" or word == "AVE" or word == "st" or word == "St" or word == "Rd") and doc[doc.index(word) -1].isdigit():
+                    digits.pop()
+            if (len(word) == 5 and word.isdigit()) or flag == 1:
+                if len(digits)>= 2:
+                    temp_index = len(digits)-1
+                    while doc.index(word) < digits[temp_index]+5 and temp_index != 0:
+                        temp_index -= 1
+                    if temp_index != 0:
+                        temp_arr = doc[digits[temp_index]:doc.index(word)+1]
+                    else:
+                        digits.append(doc.index(word))
+                        continue
+                else:
+                    if doc.index(word) >= 5:
+                        temp_arr = doc[0:doc.index(word) + 1]
+                    else:
+                        digits.append(doc.index(word))
+                        continue
                 cleaned.append(temp_arr)
+                if "MILLER" in temp_arr and "SABRINA" in temp_arr and "15000" in temp_arr:
+                    digits = [doc.index("15000")+1]
+                    continue
                 for element in temp_arr:
                     address += element + " "
                 file.write(address+"\n")
                 break
             elif any(char.isdigit() for char in word):
-                    last_digit = doc.index(word)
+                numberofLetter = 0
+                for c in word:
+                    if c.isalpha():
+                        numberofLetter += 1
+                if word.isdigit() or numberofLetter <2:
+                    digits.append(doc.index(word))
     file.close()
-    print(cleaned)
     return cleaned
-
-
 
 
 import os
