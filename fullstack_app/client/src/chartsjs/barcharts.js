0bc8726b6cdb0072a3b24f9110148b59404037a1
AlisonWonderland <dimitridawn@gmail.com>
2019-11-27 15:34:01 -0800

    Removed side nav and added chart change logic

--- fullstack_app/client/src/chartsjs/barcharts.js
+++ fullstack_app/client/src/chartsjs/barcharts.js
@@ -1,49 +1,253 @@
 import React, { Component } from 'react';
-import {Bar} from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
-class Chart extends Component{
+export class Chart extends Component{
     constructor(props){
         super(props);
         this.state = {
-            chartData:{
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                label: "Mail sent",
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.5)",
-                    "rgba(54, 162, 235, 0.5)",
-                    "rgba(255, 206, 86, 0.5)",
-                    "rgba(75, 192, 192, 0.5)",
-                    "rgba(153, 102, 255, 0.5)",
-                    "rgba(255, 159, 64, 0.5)",
-                    "rgba(105, 205, 0, 0.5)"
-              
-                  ],
-                borderColor: [
-                    "rgba(255,99,132,1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                    "rgba(105, 205, 0, 1)"
-                  ],
-                data: [0, 10, 5, 2, 20, 30, 45],
-                borderWidth: 1,
-                }]
-            }
+            // Can't really do the charts with state props. Leaving this for now. 
         }
     }
-    render(){
+
+    render() {
+        let chart = null;
+
+        if(this.props.chartTimeframe === "week") {
+            chart = <Bar 
+                data={this.getLastWeekData}
+                options={this.getOptions()} />
+        }
+        else if(this.props.chartTimeframe === "month") {
+            chart = <Line 
+                data={this.getLast30DaysData}
+                options={this.getOptions()} />
+        }
+        else if(this.props.chartTimeframe === "year") {
+            chart = <Line 
+                data={this.getLastYearData}
+                options={this.getOptions()} />
+        }
+        else if(this.props.chartTimeframe === "lifetime") {
+            chart = <Line 
+                data={this.getLifetimeData}
+                options={this.getOptions()} />
+        }
+
         return (
             <div className="chart">
-                <Bar 
-                    data={this.state.chartData}
-                    options={{maintainAspectRatio:false}} />
+                {chart}
             </div>
         )
     }
 
+    // Helper functions. Same as the ones in the original websites(non-React versions)
+
+    // add the database query to show the total.
+    getOptions = () => {
+        return {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: "Total mail sent this past " + this.props.chartTimeframe + ". Total: ",
+                fontSize: 18
+            },
+            scales: {
+                xAxes: [{
+                gridLines: {
+                    display:true
+                },
+                ticks: {
+                    fontSize: 16
+                }  
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display:true
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        fontSize: 16
+                    }   
+                }]
+            }
+        };
+    }
+
+    getLastWeekData = () => {
+        // we must query the database for 'data' in the datasets object
+        return {
+            labels: this.getLastWeekLabels(),
+            datasets: [{
+              data: [20, 10, 30, 10, 20, 10, 50],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.5)",
+                "rgba(54, 162, 235, 0.5)",
+                "rgba(255, 206, 86, 0.5)",
+                "rgba(75, 192, 192, 0.5)",
+                "rgba(153, 102, 255, 0.5)",
+                "rgba(255, 159, 64, 0.5)",
+                "rgba(105, 205, 0, 0.5)"
+          
+              ],
+              borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(105, 205, 0, 1)"
+              ],
+              borderWidth: 1
+            }]
+        };
+    }
+
+    getLast30DaysData = () => {
+        return {
+            labels: this.getLast30DaysLabels(),
+            datasets: [{
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: [20, 10, 30, 10, 20, 10, 50]
+            }]
+        };
+    }
+
+    getLastYearData = () => {
+        return {
+            labels: this.getLastYearLabels(),
+            datasets: [{
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: [0, 100, 184, 293, 400, 567, 630, 710, 885, 943, 1075, 1200]
+            }]
+        }
+    }
+
+    // getCGLabels are currently hard coded.
+    // Need to get the database start date(the day it was created) and end date(today's date).
+    getLifetimeData = () => {
+        return {
+            labels: this.getCGLabels(new Date("August 19, 2018"), new Date("August 19, 2019")),
+            datasets: [{
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: [0, 100, 184, 293, 400, 567, 630, 710, 885, 943, 1075, 1200]
+            }]
+        }
+    }
+
+    getLastWeekLabels = () => {
+        let dateLabels = [];
+        let today = new Date();
+        let date = null;
+        let dd = "";
+        let mm = "";
+        let yyyy = "";
+        let dateString = "";
+    
+        for(let i = 0; i < 7; ++i) {
+            date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 6 + i);
+            dd = String(date.getDate()).padStart(2, '0');
+            mm = String(date.getMonth() + 1).padStart(2, '0');
+            yyyy = date.getFullYear();
+            dateString = mm + '/' + dd + '/' + yyyy;
+            dateLabels.push(dateString);
+        }
+    
+        return dateLabels;
+    }
+
+    getLast30DaysLabels = () => {
+        let dateLabels = [];
+        let today = new Date();
+        let date = null;
+        let mm = "";
+        let dd = "";
+        let yyyy = "";
+        let dateString = "";
+    
+        for(let i = 0; i < 7; ++i) {
+            date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i));
+            dd = String(date.getDate()).padStart(2, '0');
+            mm = String(date.getMonth() + 1).padStart(2, '0');
+            yyyy = date.getFullYear();
+            dateString = mm + '/' + dd + '/' + yyyy;
+            dateLabels.push(dateString);
+        }
+    
+        return dateLabels;
+    }
+
+    getLastYearLabels = () => {
+        let dateLabels = [];
+        let today = new Date();
+        let date = null;
+        let mm = "";
+        let yyyy = "";
+        let dateString = "";
+    
+        for(let i = 0; i < 12; ++i) {
+            date = new Date(today.getFullYear(), today.getMonth() - 11 + i);
+            mm = String(date.getMonth() + 1).padStart(2, '0');
+            yyyy = date.getFullYear();
+            dateString = mm + '/' + yyyy;
+            dateLabels.push(dateString);
+        }
+    
+        return dateLabels;
+    }
+
+    getCGLabels = (startDate, endDate) => {
+        const oneDay = 1000 * 3600 * 24;
+        const dayDifference = (endDate - startDate) / oneDay;
+    
+        if(dayDifference <= 7) {
+            return this.cgLabelGenerator(dayDifference, startDate, endDate, dayDifference);
+        }
+        else {
+            return this.cgLabelGenerator(10, startDate, endDate, dayDifference);
+        }
+    }
+    
+    cgLabelGenerator = (numLabels, startDate, endDate, dayDifference) => {
+        let dateLabels = [];
+        let date = null;
+        let dd = "";
+        let mm = "";
+        let yyyy = "";
+        let dateString = "";
+    
+        for(let i = 0; i < numLabels - 1; ++i) {
+            date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 1 + (Math.ceil(dayDifference / numLabels - 1) * i));
+            dd = String(date.getDate()).padStart(2, '0');
+            mm = String(date.getMonth() + 1).padStart(2, '0');
+            yyyy = date.getFullYear();
+            dateString = mm + '/' + dd + '/' + yyyy;
+            dateLabels.push(dateString);
+        }
+    
+        // add endDate label. Got to guarentee the endDate is the last label
+        date = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+        dd = String(date.getDate()).padStart(2, '0');
+        mm = String(date.getMonth() + 1).padStart(2, '0');
+        yyyy = date.getFullYear();
+        dateString = mm + '/' + dd + '/' + yyyy;
+        dateLabels.push(dateString);
+    
+        return dateLabels
+    }
 }
+
+// Prop passed in through the dashboard component
+Chart.propTypes = {
+    chartTimeframe: PropTypes.string.isRequired
+};
 
 export default Chart;
--- fullstack_app/client/src/dashboard.js
+++ fullstack_app/client/src/dashboard.js
@@ -2,148 +2,179 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import ScriptTag from 'react-script-tag';
 import Helmet from 'react-helmet';
 import Barcharts from './chartsjs/barcharts'
 
 class SimpleMap extends Component {
-  state = {
-      percent_number : "+24%"
-  }
+    state = {
+        percent_number : "+24%",
+        chart_choice: "week"
+    }
+
+    componentDidMount () {
+        let script = document.createElement("script");
+        script.src = "app_dashboard.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }
+
+    weekSelected = () => {
+        this.setState({
+            chart_choice: "week"
+        })
+    }
+
+    monthSelected = () => {
+        this.setState({
+            chart_choice: "month"
+        })
+    }
+
+    yearSelected = () => {
+        this.setState({
+            chart_choice: "year"
+        })
+    }
+
+    lifetimeSelected = () => {
+        this.setState({
+            chart_choice: "lifetime"
+        })
+    }
 
   render() {
+    let chart = null;
+
+    if(this.state.chart_choice === "week") {
+        chart = (
+            <Barcharts chartTimeframe={"week"}/>
+        )
+    }
+    else if(this.state.chart_choice === "month") {
+        chart = (
+            <Barcharts chartTimeframe={"month"}/>
+        )
+    }
+    else if(this.state.chart_choice === "year") {
+        chart = (
+            <Barcharts chartTimeframe={"year"}/>
+        )
+    }
+    else if(this.state.chart_choice === "lifetime") {
+        chart = (
+            <Barcharts chartTimeframe={"lifetime"}/>
+        )
+    }
+
     return (
     <div>
         <Helmet>
-        <meta charset="utf-8"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
-        <link rel="shortcut icon" href="../favicon.ico"></link>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet"></link>
-        <link rel="stylesheet" href="styles_dashboard.css"></link>
-        <script src="https://kit.fontawesome.com/406b77ded1.js"></script>
-        <title>National Dashboard</title>
+            <meta charset="utf-8"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+            <link rel="shortcut icon" href="../favicon.ico"></link>
+            <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet"></link>
+            <link rel="stylesheet" href="styles_dashboard.css"></link>
+            <title>National Dashboard</title>
         </Helmet>
-        <div class="grid-container">
-        <div class="menu-icon">
-            <i class="fas fa-bars"></i>
-        </div>
-        <header class="header">
-            <div class="header-search">
-               
-                <form id="address-form">
-                    <input class="" id="address" type="search" placeholder="Search a city or address" aria-label="Search"></input>
-                </form>
-            </div>
-        </header>
-
-        <aside class="sidenav">
-            <div class="sidenav-close-icon">
-                <i class="fas fa-times"></i>
-            </div>
-            <span class="sidenav-title">National Dashboard</span>
-            <ul class="sidenav-list">
-                <li class="sidenav-list-item">Location Stats</li>
-                <li class="sidenav-list-item">Visualizations</li>
-                <li class="sidenav-list-item">Mail upload</li>
-            </ul>
-        </aside>
-
-        <main class="main">
-            <div class="main-header">
-                <div id="graph-picker" class="time-picker">
-                    <button>Last week</button>
-                    <button>Last month</button>
-                    <button>Last year</button>
-                    <button>Lifetime</button>
-                    <button>Custom Timeframe</button>
-                </div>
-                <Barcharts />
-            </div>
-
-            <div class="main-overview">
-                <div class="overviewcard">
-                    Top 5 cities(mail sent): city, mail sent in
-                    <ol>
-                        <li>City, # of mail sent in</li>
-                        <li>City, # of mail sent in</li>
-                        <li>City, # of mail sent in</li>
-                        <li>City, # of mail sent in</li>
-                        <li>City, # of mail sent in</li>
-                    </ol>
-                </div>
-                <div class="overviewcard">
-                    Top 5 addresses. mail sent in
-                    <ol>
-                        <li>Address, # of mail sent in</li>
-                        <li>Address, # of mail sent in</li>
-                        <li>Address, # of mail sent in</li>
-                        <li>Address, # of mail sent in</li>
-                        <li>Address, # of mail sent in</li>
-                    </ol>
-                </div>
-                <div class="overviewcard">
-                    Top 5 dates. mail sent in
-                    <ol>
-                        <li>Dates, # of mail sent in</li>
-                        <li>Dates, # of mail sent in</li>
-                        <li>Dates, # of mail sent in</li>
-                        <li>Dates, # of mail sent in</li>
-                        <li>Dates, # of mail sent in</li>
-                    </ol>
-                </div>
-            </div>
-
-            <div class="main-cards">
-                <div class="card">
-                    <canvas id="states-piechart"></canvas>
-                </div>
-                <div class="card">
-                    Mail count net change.
-                    <div id="net-picker" class="time-picker">
-                        <button>Yesterday</button>
-                        <button>Last week</button>
-                        <button>Last month</button>
-                        <button>Last year</button>
+        <div className="grid-container">
+            <main className="main">
+                <div className="main-header">
+                    <div id="graph-picker" className="time-picker">
+                        <button onClick={this.weekSelected}>Last week</button>
+                        <button onClick={this.monthSelected}>Last month</button>
+                        <button onClick={this.yearSelected}>Last year</button>
+                        <button onClick={this.lifetimeSelected}>Lifetime</button>
+                        {/* Lets not include custom for now */}
+                        {/* <button>Custom Timeframe</button> */}
                     </div>
-                    <span class="net-change-percent">
-                        <span class="sign positive" > {this.state.percent_number} </span>
-                    </span>
-                    <span class="net-change-value">
-                        <span id="net-change-msg">
-                            <span id="net-value" class="positive">+200</span> mail recieved vs. <span id="time-choice">yesterday</span>
-                        </span>
-                    </span>
+                    {chart}
                 </div>
-            </div>
 
-            <div class="main-header">
-                table with state and the total number of mail from them
-                <table>
-                    <tr>
-                        <th>State</th>
-                        <th>Mail count</th>
-                    </tr>
-                    <tr>
-                        <td>California</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <td>Nevada</td>
-                        <td>5</td>
-                    </tr>
-                </table>
-            </div>    
+                <div className="main-overview">
+                    <div className="overviewcard">
+                        Top 5 cities(mail sent): city, mail sent in
+                        <ol>
+                            <li>City, # of mail sent in</li>
+                            <li>City, # of mail sent in</li>
+                            <li>City, # of mail sent in</li>
+                            <li>City, # of mail sent in</li>
+                            <li>City, # of mail sent in</li>
+                        </ol>
+                    </div>
+                    <div className="overviewcard">
+                        Top 5 addresses. mail sent in
+                        <ol>
+                            <li>Address, # of mail sent in</li>
+                            <li>Address, # of mail sent in</li>
+                            <li>Address, # of mail sent in</li>
+                            <li>Address, # of mail sent in</li>
+                            <li>Address, # of mail sent in</li>
+                        </ol>
+                    </div>
+                    <div className="overviewcard">
+                        Top 5 dates. mail sent in
+                        <ol>
+                            <li>Dates, # of mail sent in</li>
+                            <li>Dates, # of mail sent in</li>
+                            <li>Dates, # of mail sent in</li>
+                            <li>Dates, # of mail sent in</li>
+                            <li>Dates, # of mail sent in</li>
+                        </ol>
+                    </div>
+                </div>
 
-        </main>
+                <div className="main-cards">
+                    <div className="card">
+                        <canvas id="states-piechart"></canvas>
+                    </div>
+                    <div className="card">
+                        Mail count net change.
+                        <div id="net-picker" className="time-picker">
+                            <button>Yesterday</button>
+                            <button>Last week</button>
+                            <button>Last month</button>
+                            <button>Last year</button>
+                        </div>
+                        <span className="net-change-percent">
+                            <span className="sign positive" > {this.state.percent_number} </span>
+                        </span>
+                        <span className="net-change-value">
+                            <span id="net-change-msg">
+                                <span id="net-value" className="positive">+200</span> mail recieved vs. <span id="time-choice">yesterday</span>
+                            </span>
+                        </span>
+                    </div>
+                </div>
 
-        <footer class="footer">
-            <div class="footer-copyright">&copy; AMT</div>
-            <div class="footer-signature">Made by team homeless baby bear</div>
-        </footer>
-  </div>  
-  <ScriptTag isHydrating={true} type="text/javascript" src="https://cdn.jsdelivr.net/npm/places.js@1.16.6" />
-        <ScriptTag isHydrating={true} type="text/javascript" src="app_dashboard.js" />
+                <div className="main-header">
+                    table with state and the total number of mail from them
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>State</th>
+                                <th>Mail count</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>California</td>
+                                <td>10</td>
+                            </tr>
+                            <tr>
+                                <td>Nevada</td>
+                                <td>5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>    
+
+            </main>
+
+            <footer className="footer">
+                <div className="footer-copyright">&copy; AMT</div>
+                <div className="footer-signature">Made by team homeless baby bear</div>
+            </footer>
+        </div>  
     </div>
 
     );
