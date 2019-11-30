bde0a3b2b79169386c33411b5393a9d9eb3190ce
kekohuke <43626569+kekohuke@users.noreply.github.com>
2019-11-30 11:41:52 -0800

    heatmap navbar shows

--- fullstack_app/client/src/navbar.js
+++ fullstack_app/client/src/navbar.js
@@ -70,7 +70,7 @@ const useStyles = makeStyles(theme => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(0),
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
