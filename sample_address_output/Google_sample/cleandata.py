871bf38e17d2ce04234cb67ffc6e8b671fcae6bc
992X <1024745351@qq.com>
2019-12-02 01:06:19 -0800

    finish backup data cleaning

--- sample_address_output/Google_sample/cleandata.py
+++ sample_address_output/Google_sample/cleandata.py
@@ -1,51 +1,110 @@
 import re
+
 lines = [line.rstrip('\n') for line in open('raw_data.txt')]
 file = open("classify_raw.txt", "w")
 file.write("-------------------POBOX-----------------"+"\n")
 for line in lines:
     if "Box" in line:
         file.write("\n")
-        arr = line.split()
-        arr.index("Box")
-        newline = ""
-        print(line)
-
-        if arr[arr.index("Box")-1] == "PO":
-            arr.pop(arr.index("Box")-1)
-        elif arr[arr.index("Box")-1] == "O":
-            arr.pop(arr.index("Box")-1)
-            arr.pop(arr.index("Box")-1)
-        arr.remove("Box")
-        for element in arr:
-            newline += element  + " "
-        print(newline)
-        file.write(newline + "\n")
+        word_list = line.split()
+        pos = word_list.index("Box")
+        word_list.pop(pos)
+        # remove "P O BOX" or "PO BOX"
+        if word_list[pos-1] == "PO":
+            word_list.pop(pos-1)
+        elif word_list[pos-1] == "O":
+            word_list.pop(pos-1)
+            if word_list[pos-2] == "P":
+                word_list.pop(pos-2)
+        line = ""
+        for element in word_list:
+            line += element  + " "
+        # remove email
+        if "com" in line:
+            line = line[line.find("com")+4:]
+        file.write(line + "\n")
 
 file.write("-------------------USPS-----------------"+"\n")
 for line in lines:
-    if "USPS" in line:
-        print(line)
-        file.write(line+"\n")
-file.write("-------------------ORIGIN ID-----------------"+"\n")
+    if "USPS" in line and "Box" not in line:
+        file.write("\n")
+        # remove everything before sender
+        if "FROM" in line:
+            line = line[line.find("FROM")+5:]
+        elif "PRIORITY" in line:
+            line = line[line.find("PRIORITY")+9:]
+            first_word = line.split()[0]
+            # check if part of "MAIL" remain
+            if first_word == "M" or first_word == "MA" or first_word == "MAI" or first_word == "MAIL" :
+                line = line[len(first_word)+1:]
+        # remove info of us
+        if "SHIP" in line:
+            line = line[:line.find("SHIP")]
+        file.write(line + "\n")
+
+file.write("-------------------FedEx ORIGIN ID-----------------"+"\n")
 for line in lines:
-    if "ORIGIN ID" in line:
-        print(line)
-        file.write(line+"\n")
-file.write("-------------------FedEx-----------------"+"\n")
-for line in lines:
-    if "FedEx" in line:
-        print(line)
+    if "ORIGIN ID" in line and "USPS" not in line and "Box" not in line:
+        file.write("\n")
+        line = line[line.find("ORIGIN")+28:]
+        if "UNITED" in line:
+            line = line[:line.find("UNITED")]
         file.write(line+"\n")
 
-file.write("-------------------A MILLION THANKS-----------------"+"\n")
+file.write("-------------------FedEx US Airbill-----------------"+"\n")
 for line in lines:
-    if "A MILLION THANKS" in line:
-        print(line)
+    if "FedEx" in line and "ORIGIN ID" not in line and "USPS" not in line and "Box" not in line:
+        file.write("\n")
+        # remove everything before name
+        line = line[line.find("Sender")+7:]
+        first_word = line.split()[0]
+        if first_word == "s":
+            line = line[2:]
+        # remove everything after zip
+        if "ZIP" in line:
+            line = line[:line.find("ZIP")+9]
+        # pick up first_name and last_name
+        word_list = line.split()
+        name = word_list[0] + " " + word_list[word_list.index("Name")-1]
+        if "Company" in line:
+            line = line[line.find("Company")+8:]
+        # remove everything between company name and address
+        word_list = line.split()
+        if "Address" in line:
+            pos = word_list.index("Address")
+            word_list.pop(pos)
+            for x in range(1, 5):
+                if len(word_list[pos-x]) < 4 :
+                    if word_list[pos-x] != "CO" and word_list[pos-x] != "INC":
+                        word_list.pop(pos-x)
+                    else:
+                        break
+                elif word_list[pos-x].isdigit():
+                        word_list.pop(pos-x)
+                else:
+                    break
+        if "Dept Floor Suite Room" in line:
+            pos = word_list.index("Dept")
+            for x in range(4):
+                word_list.pop(pos)
+        if "City" in line:
+            word_list.pop(word_list.index("City"))
+        if "State" in line:
+            word_list.pop(word_list.index("State"))
+        if "ZIP" in line:
+            word_list.pop(word_list.index("ZIP"))
+        line = name + " "
+        for element in word_list:
+            line += element  + " "
+        #print(line)
         file.write(line+"\n")
+
 file.write("-------------------Other-----------------"+"\n")
 for line in lines:
-    if "BOX" not in line and "USPS" not in line and "ORIGIN ID" not in line and "FedEx" not in line and "A MILLION THANKS" not in line:
-        print(line)
+    if "Box" not in line and "USPS" not in line and "ORIGIN ID" not in line and "FedEx" not in line:
+        #print(line)
+        if "SHIP" in line:
+            line = line[:line.find("SHIP")]
         arr = line.split()
         start = 0
         zipcode_place = len(arr)
