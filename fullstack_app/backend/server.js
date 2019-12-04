1fbb4e35ba5faa89a2996917b1d5a088c299d6f9
992X <1024745351@qq.com>
2019-12-03 19:44:30 -0800

    beauty backend

--- fullstack_app/backend/server.js
+++ fullstack_app/backend/server.js
@@ -23,26 +23,26 @@ const path = require('path');
 //-------------google map geocoidng api-------
 const googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyAWwGyXySCICwS8tp-Ap4OWhj7wTPVRJ0g'
-  });
+});
 //--------------------------------------------
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../client/public/uploads/')
-  },
-  filename: function (req, file, cb) {
-    crypto.pseudoRandomBytes(16, function (err, raw) {
-      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
-    });
-  }
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+        cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+        });
+    }
 });
 const upload = multer({
-  storage: storage
+    storage: storage
 }); 
 
 // this is our MongoDB database
 const dbRoute =
-  'mongodb+srv://cotest:copassword@cluster0-wxdkp.mongodb.net/test?retryWrites=true&w=majority';
+    'mongodb+srv://cotest:copassword@cluster0-wxdkp.mongodb.net/test?retryWrites=true&w=majority';
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true,  useUnifiedTopology: true  });
@@ -64,10 +64,10 @@ app.use(logger('dev'));
 // This method fetches all available data in our database
 // The following get functions used on home.js
 router.get('/getData', (req, res) => {
-  new_Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
+    new_Data.find((err, data) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
 });
 
 router.post('/getData_bydate', async (req, res) => {
@@ -83,118 +83,116 @@ router.post('/getData_bydate', async (req, res) => {
 // this is our update method
 // this method overwrites existing data in our database
 router.post('/updateData', (req, res) => {
-  const { id, update } = req.body;
-  new_Data.findByIdAndUpdate(id, update, (err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
+    const { id, update } = req.body;
+    new_Data.findByIdAndUpdate(id, update, (err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
 });
 
 router.post('/upload',upload.any(), async (req,res)=>{
-  console.log("in post");
-  try{
-    console.log("in try");
-    console.log(req.files.length);
-          for (i = 0, len = req.files.length; i < len; i++) {
-              image_text(req.files[i]["filename"]);
-          }
-      return res.json({ success: true });
-     
-  }catch(err){
-    console.log("in err")
-    return res.json({ success: false, error: err });
-      console.log("err"+err);
-      console.log("err"+err);
-      res.json({message:err});
-      res.redirect("/upload");
-  }
+    console.log("in post");
+    try{
+        console.log("in try");
+        console.log(req.files.length);
+            for (i = 0, len = req.files.length; i < len; i++) {
+                image_text(req.files[i]["filename"]);
+            }
+        return res.json({ success: true });
+        
+    }catch(err){
+        console.log("in err")
+        return res.json({ success: false, error: err });
+        console.log("err"+err);
+        console.log("err"+err);
+        res.json({message:err});
+        res.redirect("/upload");
+    }
 });
 
 const image_text = async function(file){
-  let idToBeAdded = 0;
-  new_Data.find((err, data) => {
-    let currentIds = data.map((data) => data.id);
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
-    }
-  });
-
-  const fileName = path.join(__dirname,'../client/public/uploads/'+file);
-  const [result] = await client.documentTextDetection(fileName);
-  const fullTextAnnotation = result.fullTextAnnotation;
-  //--------------clean data--------------------
-  var spawn = require("child_process").spawn;
-  var process = spawn('python',["./cleandata-fornodejs.py",fullTextAnnotation.text ]);
-  var goodaddress =fullTextAnnotation.text
-  process.stdout.on('data',function(data){
-    goodaddress=data.toString()
-    googleMapsClient.geocode({
-      address: goodaddress
-    }, function(err, res) {
-      if (!err) {
-          var formatted_address = "";
-          address_components = res.json.results[0]["address_components"]
-          let city = ""
-          let state = ""
-          for(let i= 0; i < address_components.length; i ++)
-          {
-            if( address_components[i]["types"][0] =='administrative_area_level_1')
-              state = address_components[i]["long_name"]
-            if(address_components[i]["types"][0] ==  'locality')
-              city = address_components[i]["long_name"]
-          }
-          var accu = res.json.results[0]["geometry"]["location_type"];//is "ROOFTOP" or not
-          if (accu == "ROOFTOP"){
-            formatted_address = res.json.results[0]["formatted_address"];
-            const doc = new new_Data({id: idToBeAdded,city: city, state: state, picture: '/'+ file, address: formatted_address, accuracy: accu,lat:res.json.results[0]["geometry"]["location"].lat, lng:res.json.results[0]["geometry"]["location"].lng});
-            doc.save();
-          }
-          else{
-            const raw = new new_Data({id: idToBeAdded,city: city, state: state,  picture: '/'+ file, address: goodaddress, accuracy: accu,lat:res.json.results[0]["geometry"]["location"].lat, lng:res.json.results[0]["geometry"]["location"].lng});
-            raw.save();
-          }
-          doc.save();
-      }
+    // generate next avaible id
+    let idToBeAdded = 0;
+    new_Data.find((err, data) => {
+        let currentIds = data.map((data) => data.id);
+        while (currentIds.includes(idToBeAdded)) {
+        ++idToBeAdded;
+        }
     });
-  
-  
-  });
-  //-----------------------------------------
+    // using google handwring api to capture words
+    const fileName = path.join(__dirname,'../client/public/uploads/'+file);
+    const [result] = await client.documentTextDetection(fileName);
+    const fullTextAnnotation = result.fullTextAnnotation;
+    //--------------clean data--------------------
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./cleandata-fornodejs.py", fullTextAnnotation.text]);
+    var goodaddress =fullTextAnnotation.text
+    process.stdout.on('data',function(data){
+        goodaddress=data.toString()
+        googleMapsClient.geocode({address: goodaddress}, function(err, res) {
+            if (!err) {
+                var formatted_address = "";
+                address_components = res.json.results[0]["address_components"]
+                let city = ""
+                let state = ""
+                for(let i= 0; i < address_components.length; i ++)
+                {
+                    if( address_components[i]["types"][0] =='administrative_area_level_1')
+                    state = address_components[i]["long_name"]
+                    if(address_components[i]["types"][0] ==  'locality')
+                    city = address_components[i]["long_name"]
+                }
+                var accuracy = res.json.results[0]["geometry"]["location_type"];//is "ROOFTOP" or not
+                if (accuracy == "ROOFTOP"){
+                    formatted_address = res.json.results[0]["formatted_address"];
+                    const doc = new new_Data({id: idToBeAdded, city: city, state: state,picture: '/'+ file, 
+                        address: formatted_address, accuracy: accuracy, lat: res.json.results[0]["geometry"]["location"].lat, 
+                        lng: res.json.results[0]["geometry"]["location"].lng});
+                    doc.save();
+                }
+                else{
+                    const raw = new new_Data({id: idToBeAdded, city: city, state: state, picture: '/'+ file, 
+                        address: goodaddress, accuracy: accuracy, lat: res.json.results[0]["geometry"]["location"].lat, 
+                        lng: res.json.results[0]["geometry"]["location"].lng});
+                    raw.save();
+                }
+                doc.save();
+            }
+        });
+    });
 }
 
 // this is our delete method
 // this method removes existing data in our database
 router.delete('/deleteData', (req, res) => {
-  const { id } = req.body;
-  new_Data.findByIdAndRemove(id, (err) => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
+    const { id } = req.body;
+    new_Data.findByIdAndRemove(id, (err) => {
+        if (err) return res.send(err);
+        return res.json({ success: true });
+    });
 });
 
 // this is our create methid
 // this method adds new data in our database
 router.post('/putData', (req, res) => {
-  let data = new new_Data();
-
-  const { id, picture, address, raw_address, accuracy, date } = req.body;
-
-  if ((!id && id !== 0) || !picture) {
-    return res.json({
-      success: false,
-      error: 'INVALID INPUTS',
+    let data = new new_Data();
+    const { id, picture, address, raw_address, accuracy, date } = req.body;
+    if ((!id && id !== 0) || !picture) {
+        return res.json({
+        success: false,
+        error: 'INVALID INPUTS',
+        });
+    }
+    data.id = id;
+    data.picture= picture;
+    data.address = address;
+    data.raw_address = raw_address;
+    data.accuracy = accuracy;
+    data.date = date;
+    data.save((err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
     });
-  }
-  data.id = id;
-  data.picture= picture;
-  data.address = address;
-  data.raw_address = raw_address;
-  data.accuracy = accuracy;
-  data.date = date;
-  data.save((err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
 });
 
 // append /api for our http requests
