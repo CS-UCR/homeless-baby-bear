15821a2f5470382ec89956b9f4a965c5259af4ac
kekohuke <43626569+kekohuke@users.noreply.github.com>
2019-12-11 22:47:19 -0800

    try

--- fullstack_app/client/src/dashboard.js
+++ fullstack_app/client/src/dashboard.js
@@ -64,6 +64,16 @@ class SimpleMap extends Component {
         });
     };
 
+    getDataFromDbDate2 =  (fromDate, toDate, var_name) => {
+        axios.post(process.env.REACT_APP_API+'/getData_bydate', {
+            fromDate: fromDate,
+            toDate: toDate,
+            location_type: "ALL"
+        }).then((res) => {
+            var_name = res.data.data.length;
+        });
+    };
+
     // ---Label functions for mail count chart
     getLastWeekLabels (){
         let dateLabels = [];
@@ -82,7 +92,6 @@ class SimpleMap extends Component {
             dateString = mm + '/' + dd + '/' + yyyy;
             dateLabels.push(dateString);
             this.getDataFromDbDate(date,new Date(today.getFullYear(), today.getMonth(), today.getDate() - 5 + i), this.state.week, i)
-            this.getDataFromDbDate(new Date(today.getFullYear(), today.getMonth(), today.getDate() - 4-7 + i),new Date(today.getFullYear(), today.getMonth(), today.getDate() - 5-7 + i), this.state.last_week, i)
             if(this.state.getWeek !== false && i === 6)
                 this.setState({getWeek: true});
         }
@@ -110,10 +119,10 @@ class SimpleMap extends Component {
             dateLabels.push(dateString);
             if(this.state.getMonth === false && i < 6){
                 this.getDataFromDbDate(date,new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)+5),this.state.month,i)
-                this.getDataFromDbDate(new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)-30),new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)+5-30), this.state.last_month, i)
+                
             }else{
                 this.getDataFromDbDate(date,new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)+5),this.state.month,i)
-                this.getDataFromDbDate(new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)-30),new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + (5 * i)+5-30), this.state.last_month, i)
+        
                 this.setState({getMonth: true})
             }
         }
@@ -136,10 +145,10 @@ class SimpleMap extends Component {
             dateLabels.push(dateString);
             if(this.state.getYear === false && i !== 11){
                 this.getDataFromDbDate(date,new Date(today.getFullYear(), today.getMonth() - 11 + i+1),this.state.year ,i)
-                this.getDataFromDbDate(new Date(today.getFullYear()-1, today.getMonth() - 11 + i),new Date(today.getFullYear()-1, today.getMonth() - 11 + i+1),this.state.last_year ,i)
+               
             }else{
                 this.getDataFromDbDate(date,new Date(today.getFullYear(),today.getMonth() - 11 + i+1),this.state.year ,i)
-                this.getDataFromDbDate(new Date(today.getFullYear()-1, today.getMonth() - 11 + i),new Date(today.getFullYear()-1, today.getMonth() - 11 + i+1),this.state.last_year ,i)
+               
                 this.setState({getYear: true})
             }
         }
@@ -224,9 +233,9 @@ class SimpleMap extends Component {
                 this.setState({month_num: this.state.month.reduce((a, b) => a + b, 0)})
                 this.setState({year_num: this.state.year.reduce((a, b) => a + b, 0)})
                 this.setState({lifetime_num: this.state.lifetime.reduce((a, b) => a + b, 0)})
-                this.setState({last_week_num: this.state.last_week.reduce((a, b) => a + b, 0)})
-                this.setState({last_month_num: this.state.last_month.reduce((a, b) => a + b, 0)})
-                this.setState({last_year_num: this.state.last_year.reduce((a, b) => a + b, 0)})
+                this.setState({last_week_num: this.getDataFromDbDate2(Date(today.getFullYear(), today.getMonth(), today.getDate() - 14),Date(today.getFullYear(), today.getMonth(), today.getDate() - 7),last_week_num)})
+                this.setState({last_month_num: this.getDataFromDbDate2(Date(today.getFullYear(), today.getMonth()-2, today.getDate()),Date(today.getFullYear(), today.getMonth()-1, today.getDate()),last_month_num)})
+                this.setState({last_year_num: this.getDataFromDbDate2(Date(today.getFullYear()-2, today.getMonth(), today.getDate()),Date(today.getFullYear()-1, today.getMonth(), today.getDate()),last_year_num)})
                 var mapper_date = this.state.allData.map(data => data.date.substring(0,10))
                 var mapper_city = this.state.allData.map(data => data.city)
                 var mapper_address = this.state.allData.map(data => data.address)
